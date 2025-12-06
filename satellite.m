a329856b0d19e2b615ba2b9807ce34f13c0d7162
trystonfreeman <trystonfreeman@gmail.com>
2025-12-06 17:03:10 -0500

    Fixed Orbit Propagation and plotted all sat trajectories

--- satellite.m
+++ satellite.m
@@ -8,10 +8,8 @@ classdef satellite
         omega
         omega_dot
         u
-        r_0_PQW
-        r_PQW
-        v_PQW
-        v_0_PQW
+        r
+        v
         n
         T
     end
@@ -22,25 +20,32 @@ classdef satellite
             obj.omega = omega;
             obj.u = u;
             
-            r_vec = get_PQW(obj);
-            obj.r_0_PQW = r_vec(1:3);
-            obj.v_0_PQW = r_vec(4:6);
-            obj.r_PQW = obj.r_0_PQW;
-            obj.v_PQW = obj.v_0_PQW;
+            [obj.r,obj.v] = get_IJK(obj);
+            
             obj.n = sqrt(obj.mu/obj.a^3);
             obj.omega_dot = -1.5 * obj.J2*obj.n*(obj.R/obj.a)^2*cosd(obj.i);
             obj.T = 2*pi*sqrt(obj.a^3/obj.mu);
             
         end
         function obj = propagate(obj,dt)
-            obj.u = obj.u+dt/obj.T;
-            obj.omega = obj.omega + obj.omega_dot*dt;
+
+            obj.u = obj.u + (dt/obj.T)*360;
+            obj.omega = obj.omega + (obj.omega_dot)*(180/pi)*dt;
+            [obj.r,obj.v] = get_IJK(obj);
         end
 
-        function r_vec = get_PQW(obj)
-            v_PQW = sqrt(obj.mu/obj.a) * [-sind(obj.u); cosd(obj.u); 0];
-            r_PQW = [obj.a*cosd(obj.u);obj.a*sind(obj.u);0];
-            r_vec = [r_PQW;v_PQW];
+        function [r,v] = get_IJK(obj)
+            R3_omega = [cosd(-obj.omega), sind(-obj.omega), 0;...
+                        sind(-obj.omega),-cosd(-obj.omega), 0;...
+                        0              , 0                1];
+            R3_u = [cosd(-obj.u), sind(-obj.u), 0;...
+                    sind(-obj.u),-cosd(-obj.u), 0;...
+                    0          , 0          , 1];
+            R1_i = [1 0 0;...
+                    0 cosd(-obj.i), sind(-obj.i);...
+                    0 sind(-obj.i),-cosd(-obj.i)];
+            r = R3_omega*R1_i*R3_u*[obj.a;0;0];
+            v = R3_omega*R1_i*R3_u*[0; sqrt(obj.mu/obj.a); 0];
         end
     end
 end
