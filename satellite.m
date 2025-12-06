64e2fc8e5810ecf8b0177241c94f89879e11e86c
Austin-Hewitt <amh00039@mix.wvu.edu>
2025-12-06 17:32:05 -0500

    tried finding h. its not working :(

--- satellite.m
+++ satellite.m
@@ -1,26 +1,8 @@
 classdef satellite
     properties
-        a
-        i
-        omega
-        u
-        r
-        r_0
-        v
-        v_0
+        
     end
     methods
-        function obj = satellite(a,i,omega,u)
-            obj.a = a;
-            obj.i = i;
-            obj.omega = omega;
-            obj.u = u;
-        end
-        function propagate(obj)
-            
-        end
-        function maneuver(obj)
-            
-        end
+        
     end
 end
